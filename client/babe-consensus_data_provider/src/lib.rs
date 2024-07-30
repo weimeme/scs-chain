3b9fde1f860006b6acff073827df67884ee8ca77
weimeme <weimeme067@gmail.com>
2024-07-30 16:46:52 +0800

    update babe-consensus_data_provider

--- client/babe-consensus_data_provider/src/lib.rs
+++ client/babe-consensus_data_provider/src/lib.rs
@@ -5,16 +5,44 @@ use std::sync::Arc;
 use sp_keystore::{Keystore, KeystorePtr};
 use sp_consensus_babe::{AuthorityId, AuthorityPair, Randomness, Slot, VrfSignature, make_vrf_transcript, BabeApi};
 use std::marker::PhantomData;
+use sp_api::{ApiExt, ApiRef, Core, ProvideRuntimeApi};
 use sc_service::{
     error::Error as ServiceError, ChainSpec, Configuration, PartialComponents, TFullBackend,
     TFullClient, TaskManager,
 };
+use sc_client_api::{
+    backend::{AuxStore, Backend, StorageProvider},
+    UsageProvider,
+};
 use fc_rpc::pending::ConsensusDataProvider;
+use sp_runtime::{
+    generic::{Digest, DigestItem},
+    traits::{Block as BlockT, Header as HeaderT, One},
+    TransactionOutcome,
+};
 
-// /// Consensus data provider for Babe.
-// pub struct AuraConsensusDataProvider<B, C> {
-//     // slot duration
-//     slot_duration: SlotDuration,
-//     // phantom data for required generics
-//     _phantom: PhantomData<(B, C)>,
-// }
+
+/// Consensus data provider for Babe.
+/// fixme: why aura need slot duration???
+pub struct BabeConsensusDataProvider<B, C> {
+    client: Arc<C>,
+    keystore: Arc<dyn Keystore>,
+    _phantom: PhantomData<(B)>,
+}
+
+impl<B, C> BabeConsensusDataProvider<B, C>
+where
+    B: BlockT,
+    C: AuxStore + ProvideRuntimeApi<B> + UsageProvider<B>,
+    C::Api: BabeApi<B>,
+
+{
+    pub fn new(client: Arc<C>, keystore: Arc<dyn Keystore>) -> Self {
+        Self {
+            client,
+            keystore,
+            _phantom: Default::default(),
+        }
+    }
+}
+
